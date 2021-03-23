2af961c68b85ee56d12b4a5c4a1e29e011945179
hanyu <ge69vab@mytum.de>
2021-03-23 13:08:50 +0100

    removed the visualizer for now

--- include/shape_registration/preprocessing.hpp
+++ include/shape_registration/preprocessing.hpp
@@ -10,16 +10,22 @@ using PointCloudT = pcl::PointCloud<PointT>;
 namespace Preprocessing {
 
 /**
- * @brief pass_through_filter Applies pass through filter to the cloud and removes the points out of the range defined for
- * dimension.
- * @param input_cloud Input pointcloud that we want to apply filtering
- * @return filtered pointcloud
+ * @brief pass_through_filter
+ * @param input_cloud
+ * @param x_min_val
+ * @param x_max_val
+ * @param y_min_val
+ * @param y_max_val
+ * @param z_min_val
+ * @param z_max_val
+ * @return filtered cloud
  */
 PointCloudT::Ptr pass_through_filter(PointCloudT::Ptr &input_cloud, float x_min_val, float x_max_val, float y_min_val, float y_max_val, float z_min_val, float z_max_val);
 
 /**
  * @brief voxel_grid_downsampling
  * @param input_cloud
+ * @param voxel_size
  * @return
  */
 PointCloudT::Ptr voxel_grid_downsampling(PointCloudT::Ptr &input_cloud, float voxel_size);
@@ -39,11 +45,6 @@ PointCloudT::Ptr statistical_filtering(PointCloudT::Ptr &input_cloud);
  */
 PointCloudT::Ptr extract_plane(PointCloudT::Ptr &input_cloud, double threshold);
 
-/**
- * @brief visualize_point_cloud
- * @param input_cloud
- */
-void visualize_point_cloud(const PointCloudT &input_cloud);
 
 }
 
